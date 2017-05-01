dcc39829762e1f34d8b12b8cf3816d58d09e53ed
uno <st9007a@gmail.com>
2017-05-01 16:37:41 +0800

    add mechine class

---
+++ Assets/Scripts/Data/Machine.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Machine {
+
+	public enum Display {
+		Show,
+		Hide,
+		Forbidden,
+	}
+
+	public enum Status {
+		Normal,
+		Crash,
+		PowerOff,
+	}
+
+	public int id { set; get; }
+	public string name { set; get; }
+	public Display display { set; get; }
+	public Status status { set; get; }
+
+	public Dictionary<int, Machine> connection = new Dictionary<int, Machine> ();
+
+	public Machine () {
+		id = 0;
+		name = "Machine";
+		display = Display.Show;
+		status = Status.Normal;
+	}
+
+	public Machine (int id, string name, Display display, Status status) {
+		this.id = id;
+		this.name = name;
+		this.display = display;
+		this.status = status;
+	}
+
+	public void addConnection (int id, Machine m) {
+		connection.Add (id, m);
+	}
+
+	public void removeConnection (int id) {
+		connection.Remove (id);
+	}
+
+}
