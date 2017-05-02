59d9c87ff68e04061f4928a38d1e13e22849fb0e
uno <st9007a@gmail.com>
2017-05-02 08:33:48 +0800

    refactor connect

---
+++ Assets/Scripts/Machine.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Machine : MonoBehaviour {
+
+    public GameObject ConnectLine;
+    public GameObject DEBUG;
+
+    public int Id;
+    public string MachineName;
+
+    private Dictionary<int, Machine> Connection = new Dictionary<int, Machine>();
+
+    // Use this for initialization
+    void Start() {
+        if (DEBUG != null) {
+            Connect(DEBUG);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Connect (GameObject machine) {
+        Machine m = machine.GetComponent<Machine>();
+        Connection.Add(m.Id, m);
+        DrawConnectLine(machine);
+    }
+
+    void Disconnect (GameObject machine) {
+        Machine m = machine.GetComponent<Machine>();
+        Connection.Remove(m.Id);
+        RemoveConnectLine(machine);
+    }
+
+    private void DrawConnectLine(GameObject machine) {
+        GameObject line = Instantiate(ConnectLine);
+
+        line.transform.parent = gameObject.transform;
+        line.GetComponent<LineRenderer>().SetPosition(0, gameObject.transform.position);
+        line.GetComponent<LineRenderer>().SetPosition(1, machine.transform.position);
+
+        machine.transform.parent = line.transform;
+    }
+
+    private void RemoveConnectLine(GameObject machine) {
+        Destroy(machine.transform.parent.gameObject);
+    }
+}
