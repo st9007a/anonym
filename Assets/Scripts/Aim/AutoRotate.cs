1ae47407ed204d15a57ebcb48c022addf7d59074
uno <st9007a@gmail.com>
2017-05-21 18:00:21 +0800

    fix aim

---
+++ Assets/Scripts/Aim/AutoRotate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour {
+
+    public float Speed = 120.0f;
+    public Vector3 Axis;
+
+    private Vector3 rotate;
+    private float speed;
+
+    void Start() {
+        speed = Speed;
+    }
+
+    void Update () {
+        transform.Rotate(Axis * Time.deltaTime * speed);
+	}
+
+    public void StopRotate() {
+        speed = 0;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public void ResetRotate() {
+        speed = Speed;
+    }
+}
