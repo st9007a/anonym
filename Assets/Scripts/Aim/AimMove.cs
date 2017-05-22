b2d5a2e068fd3a073d096042f4749c628ee5b347
uno <st9007a@gmail.com>
2017-05-22 09:36:44 +0800

    adjust aim movement and add lock object

--- Assets/Scripts/Aim/AimMove.cs
+++ Assets/Scripts/Aim/AimMove.cs
@@ -4,34 +4,55 @@ using UnityEngine;
 
 public class AimMove : MonoBehaviour {
 
+    public float PositionOffset = 0.375f;
+
+    private Vector3 _nowMachinePos;
+    private GameManager _manager;
+    private GameObject _camera;
+
 	void Start () {
-        GameObject manager = GameObject.Find("GameManager");
-        GameObject mainCamera = GameObject.Find("Main Camera");
-        if (manager == null || mainCamera == null) {
+        _manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        _camera = GameObject.Find("Main Camera");
+
+        if (_manager == null || _camera == null) {
             Debug.LogError("GameObject not found.");
             return;
         }
 
-        transform.position = Vector3.Lerp(manager.GetComponent<GameManager>().AllMachines[0].transform.position, mainCamera.transform.position, 0.375f);
-	}
+        _nowMachinePos = _manager.AllMachines[0].transform.position;
+        transform.position = Vector3.Lerp(_nowMachinePos, _camera.transform.position, PositionOffset);
+        
+    }
 	
 	void Update () {
 
         if (Input.GetKeyDown("up"))
         {
+            _nowMachinePos = GetNeighboringMachinePos(_nowMachinePos, Vector3.forward);
+            transform.position = Vector3.Lerp(_nowMachinePos, _camera.transform.position, PositionOffset);
         }
         else if (Input.GetKeyDown("left"))
         {
-
+            _nowMachinePos = GetNeighboringMachinePos(_nowMachinePos, Vector3.left);
+            transform.position = Vector3.Lerp(_nowMachinePos, _camera.transform.position, PositionOffset);
         }
         else if (Input.GetKeyDown("down"))
         {
-
+            _nowMachinePos = GetNeighboringMachinePos(_nowMachinePos, Vector3.back);
+            transform.position = Vector3.Lerp(_nowMachinePos, _camera.transform.position, PositionOffset);
         }
         else if (Input.GetKeyDown("right"))
         {
-
+            _nowMachinePos = GetNeighboringMachinePos(_nowMachinePos, Vector3.right);
+            transform.position = Vector3.Lerp(_nowMachinePos, _camera.transform.position, PositionOffset);
         }
 		
 	}
+
+    Vector3 GetNeighboringMachinePos(Vector3 aimMachinePos, Vector3 dir)
+    {
+        Vector3 pos = _nowMachinePos;
+
+        return pos;
+    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -67,4 +67,5 @@ public class GameManager : MonoBehaviour {
             AllMachines[i].GetComponent<Machine>().StartConnect();
         }
     }
+
 }
