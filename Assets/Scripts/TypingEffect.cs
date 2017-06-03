1bd3394745838145188f59bd56413f78973fc0fb
uno <st9007a@gmail.com>
2017-06-03 20:30:58 +0800

    ui

---
+++ Assets/Scripts/TypingEffect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class TypingEffect : MonoBehaviour {
+
+    public float Delay;
+    public float Speed;
+
+    TextMeshProUGUI Txt;
+    string Content;
+
+	void Awake () {
+        Txt = GetComponent<TextMeshProUGUI>();
+        SetContent(Txt.text);
+	}
+
+    public void SetContent(string content) {
+        Content = content;
+        Txt.text = "";
+        StartCoroutine("Typing");
+    }
+
+    IEnumerator Typing() {
+
+        yield return new WaitForSeconds(Delay);
+
+        foreach (char c in Content) {
+            Txt.text += c;
+            yield return new WaitForSeconds(Speed);
+        }
+    }
+}
